c5dfc80f89998572289d1ba0bb8960e0a71b4027
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-01-07 11:46:45 +0530

    l1

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello World from Express!');
+});
+
+app.get('/name', (req, res) => {
+    res.send("<h1>Pasan</h1>");
+    
+  });
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
